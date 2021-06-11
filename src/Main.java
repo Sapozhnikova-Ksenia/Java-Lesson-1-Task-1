cc7ca37d895c17da11da82d79adef6363564d6b4
Sapozhnikova-Ksenia <mobillka92@gmail.com>
2021-06-11 20:56:39 +0300

    16 number card with space fail

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         // TODO: подставлять номер карты нужно сюда между двойными кавычками, без пробелов
-        String number = "40452390208807530335";
+        String number = "4929 4921 6749 2791";
         System.out.println(String.format("Result is %s", isValidCardNumber(number) ? "OK" : "FAIL"));
     }
 
