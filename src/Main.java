010200ac19c3d6b1c8afc898026cd6cf77e8bfc0
Sapozhnikova-Ksenia <mobillka92@gmail.com>
2021-06-11 20:34:55 +0300

    14 number card fail

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         // TODO: подставлять номер карты нужно сюда между двойными кавычками, без пробелов
-        String number = "4929492167492";
+        String number = "36702532303210";
         System.out.println(String.format("Result is %s", isValidCardNumber(number) ? "OK" : "FAIL"));
     }
 
